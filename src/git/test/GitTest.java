8ea3bdb5292f95f70a6833cd5e2e6c70fb924acd
Admin <Admin@10.2.84.108>
2017-05-11 16:57:29 -0700

    Human generated file

--- src/git/test/GitTest.java
+++ src/git/test/GitTest.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package git.test;
 
-/**
- *
- * @author Admin
- */
-public class GitTest {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+
+public class GitTest{
+
+    public static void main(String[]args){
     
+    
+        
+            System.out.println("Its working");
+    
+    
+    }
+
 }
